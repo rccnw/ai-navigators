1618ea051b628528b0e22e09d3f5cfa0663f9245
rccnw <rcc@pobox.com>
2024-07-03 21:21:10 -0700

    landing page

--- ai-navigators/Program.cs
+++ ai-navigators/Program.cs
@@ -2,10 +2,18 @@ using ai_navigators;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
